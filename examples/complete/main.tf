ef8c778afd3eaffd0a79a1476c4717f5c8348af0
Jimish Fotariya <fotariyajimish@gmail.com>
2024-09-04 15:39:43 +0530

    fix: kms context enablement

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -10,7 +10,6 @@ module "eventbus_kms_key" {
   source     = "registry.terraform.io/SevenPicoForks/kms-key/aws"
   version    = "2.0.0"
   context    = module.example_context.self
-  enabled    = module.context.enabled
   attributes = ["kms", "key"]
 
   customer_master_key_spec = "SYMMETRIC_DEFAULT"
