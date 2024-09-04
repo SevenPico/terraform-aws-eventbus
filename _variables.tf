56efc4cbe56d40075460e71c16d9321c954725c9
Jimish Fotariya <fotariyajimish@gmail.com>
2024-09-04 15:04:55 +0530

    fix: variable desc

--- _variables.tf
+++ _variables.tf
@@ -21,14 +21,16 @@
 
 variable "eventbus_name" {
   type = string
+  description = "(Required) The name of the new event bus. The names of custom event buses can't contain the / character. To create a partner event bus, ensure the name matches the event_source_name"
 }
 
 variable "kms_key_identifier" {
   type    = string
-  default = ""
+  default = "Optional) The identifier of the AWS KMS customer managed key for EventBridge to use, if you choose to use a customer managed key to encrypt events on this event bus. The identifier can be the key Amazon Resource Name (ARN), KeyId, key alias, or key alias ARN."
 }
 
 variable "event_source_name" {
   type    = string
-  default = ""
+  description = ""
+  default = "(Optional) The partner event source that the new event bus will be matched with. Must match eventbus_name."
 }
--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,6 +1,30 @@
+module "example_context" {
+  source     = "registry.terraform.io/SevenPico/context/null"
+  version    = "2.0.0"
+  context    = module.context.self
+  enabled    = module.context.enabled
+  attributes = []
+}
+
+module "eventbus_kms_key" {
+  source     = "registry.terraform.io/SevenPicoForks/kms-key/aws"
+  version    = "2.0.0"
+  context    = module.example_context.self
+  enabled    = module.context.enabled
+  attributes = ["kms", "key"]
+
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  description              = "KMS key for ${module.example_context.id}"
+  key_usage                = "ENCRYPT_DECRYPT"
+  multi_region             = false
+  policy                   = ""
+}
+
+
 module "eventbus" {
   source  = "../../"
   context = module.context.tags
 
+  kms_key_identifier = module.eventbus_kms_key.key_arn
   eventbus_name = "domain"
 }
