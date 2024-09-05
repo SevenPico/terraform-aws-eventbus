78b36f1459afb02f86403f341f37871b57db9173
Jimish Fotariya <fotariyajimish@gmail.com>
2024-09-05 18:53:32 +0530

    fix: changed resource to *

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "eventbus_policy" {
     actions = [
       "events:PutEvents",
     ]
-    resources = ["${local.arn_prefix}:events:${local.region}:${local.account_id}:event-bus/${local.eventbus_name}"]
+    resources = ["*"]
 
     principals {
       type        = "AWS"
